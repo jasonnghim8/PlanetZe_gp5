47f7188f80f1032b0e7bde837c8915789e18c332
jasonnghim8 <jasonlokhim.ng@mail.utoronto.ca>
2024-11-10 19:24:48 -0500

    updated current progress

---
+++ app/src/main/java/com/example/planetZe_gp5/acfResults.java
@@ -0,0 +1,29 @@
+package com.example.planetZe_gp5;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class acfResults extends AppCompatActivity {
+    TextView totalAcf;
+
+}
